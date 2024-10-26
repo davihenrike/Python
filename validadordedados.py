f7241d25c74a6aa33edcdb5fe8ba30acd6609ca2
Davi Henrique <60934041+davihenrike@users.noreply.github.com>
2024-10-26 16:07:12 -0300

    Update validadordedados.py
    Código melhorado

--- validadordedados.py
+++ validadordedados.py
@@ -1,4 +1,4 @@
-sexo = str(input('Informe seu sexo [M/F]:')).strip().upper()[0]
-while sexo not in  'MFmf':
-    sexo = str(input('Dados inválidos, por favor informe seu genero com M para masculino e F para feminino.')).strip().upper()[0]
+sexo = (input('Informe seu sexo [M/F]:')).strip().upper()[0]
+while sexo not in  'MF':
+    sexo = (input('Dados inválidos, por favor informe seu genero com M para masculino e F para feminino.')).strip().upper()[0]
 print(f'Sexo {sexo} registrado com sucesso')
